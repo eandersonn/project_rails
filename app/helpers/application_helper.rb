c86685806d36a69286b0e47f5204f5b5d5dfe76b
Erin Anderson <e.anderson1226@gmail.com>
2021-06-12 16:45:52 -0400

    Creating all the helper methods for this project. With checking the current patient status with the method current_patient as well as making sure that the patient is logged in through the sessions id. Lastly redirecting the patient/user to the welcome page if they have yet to log in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+    private
+    def current_patient
+        if session[:patient_id]
+            @current_patient
+        else
+            @current_patient = Patient.find_by(session[:patient_id])
+        end
+    end
+
+    def logged_in?
+        !!session[:patient_id]
+    end
+
+    def redirect_if_not_Logged_in
+        if !logged_in?
+            redirect_to '/'
+        end
+    end
+
 end
