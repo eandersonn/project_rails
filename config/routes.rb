a9bda72023f9b0ec2cb644e95cd7e4452f32bc58
Erin Anderson <e.anderson1226@gmail.com>
2021-06-12 21:21:40 -0400

    Adding the new patient signup routes as well as the login and logout routes from the sessions conventional routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,19 @@ Rails.application.routes.draw do
   #home page
   get '/' => 'sessions#welcome'
 
+  #signup
+  get '/signup' => 'patients#new'
+  post '/signup' => 'patients#create'
+
+  #login/logout
+  get '/login' => 'sessions#new'
+  post'/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :categories
   resources :messages
   resources :patients
   resources :appointments
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
