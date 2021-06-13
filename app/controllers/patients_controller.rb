df380358c88c4cf7937bec57b557e26fd5b116c4
Erin Anderson <e.anderson1226@gmail.com>
2021-06-12 21:30:22 -0400

    Adding the necessary routes to the patientController where new creates a new patient instance. While create method creates a new local object and checks that its saved and directes it to the patient_path which would be the show.erb. Then if that patient wasnt' found then it would render to the new.erb for the patient to signup. The show method finds the patiend with its id params. if the program can't find the patient it will redirect to the welcome page. Lastly to make the code dry we created a private method that permits the param values.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,29 @@
 class PatientsController < ApplicationController
+
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.new(patient_params)
+        if @patient.save
+            session[:patient_id] = @patient_id
+            redirect_to @patient
+        else
+            render :new
+        end
+    end
+
+    def show
+        @patient = Patient.find_by_id(params[:id])
+        if !@patient
+            redirect_to '/'
+        end
+    end
+
+    private
+    def patient_params
+        params.require(:patient).permit(:first_name, :last_name, :email, :address, :password)
+    end
+
 end
