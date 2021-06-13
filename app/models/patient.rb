b2628e49b0743d3c504b3146ee6b43ce5db27938
Erin Anderson <e.anderson1226@gmail.com>
2021-06-12 21:22:43 -0400

    Adding the validations to the Patient model where the patient's first name and last name must be filled out as well as the email with it being unique and not the same as another patient.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,9 @@
 class Patient < ApplicationRecord
+    has_many :messages
+    has_many :messaged_appointments, through: :messages, source: :appointment
+    has_secure_password
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true, presence: true
 end
